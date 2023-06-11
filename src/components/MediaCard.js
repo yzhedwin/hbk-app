cfc78fe50615017aa8a81e1c6304a68a25e3dc2f
Edwin <zizo_edwin@hotmail.com>
2023-06-11 23:41:31 +0800

    Add alert when item is added to cart

--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -4,16 +4,36 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
+import { IconButton, Snackbar } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function MediaCard(props) {
-  const {order} = props
+  const { order } = props;
+  const [open, setOpen] = React.useState(false);
+  const handleClick = () => {
+    setOpen(true);
+  };
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
   return (
     <Card sx={{ maxWidth: 100 }}>
-      <CardMedia
-        sx={{ height: 100 }}
-        image={order?.url}
-        title={order?.name}
-      />
+      <CardMedia sx={{ height: 100 }} image={order?.url} title={order?.name} />
       <CardContent className="menu-card-content">
         <div>{order?.name}</div>
         <div>Price:{order?.price}</div>
@@ -21,10 +41,20 @@ export default function MediaCard(props) {
       <CardActions className="menu-card-action">
         <Button
           size="small"
-          onClick={() => props?.handleAddItem(order)}
+          onClick={() => {
+            handleClick();
+            props?.handleAddItem(order);
+          }}
         >
           Add
         </Button>
+        <Snackbar
+          open={open}
+          autoHideDuration={1000}
+          onClose={handleClose}
+          message="Added to cart"
+          action={action}
+        />
       </CardActions>
     </Card>
   );
