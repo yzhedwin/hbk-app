79ad37ed72c4014e82d589ae05063c65873a172a
yzhedwin <zizo_edwin@hotmail.com>
2023-07-09 12:39:00 +0800

    Change menu item layout

--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -4,8 +4,8 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
-import { IconButton, Snackbar } from "@mui/material";
-import CloseIcon from '@mui/icons-material/Close';
+import { Box, Container, Grid, IconButton, Snackbar } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function MediaCard(props) {
   const { order } = props;
@@ -32,30 +32,37 @@ export default function MediaCard(props) {
     </React.Fragment>
   );
   return (
-    <Card sx={{ maxWidth: 100 }}>
-      <CardMedia sx={{ height: 100 }} image={order?.url} title={order?.name} />
-      <CardContent className="menu-card-content">
-        <div>{order?.name}</div>
-        <div>Price:{order?.price}</div>
-      </CardContent>
-      <CardActions className="menu-card-action">
-        <Button
-          size="small"
-          onClick={() => {
-            handleClick();
-            props?.handleAddItem(order);
-          }}
-        >
-          Add
-        </Button>
-        <Snackbar
-          open={open}
-          autoHideDuration={1000}
-          onClose={handleClose}
-          message="Added to cart"
-          action={action}
+    <Card sx={{ width: "100%" }}>
+      <div style={{display: 'flex', flexDirection: "row"}}>
+        <CardMedia
+          sx={{ height: 200, width: 250 }}
+          image={order?.url}
+          title={order?.name}
         />
-      </CardActions>
+        <CardContent className="menu-card-content" style={{flex: 1, alignSelf: "center"}}>
+          <div>{order?.name}</div>
+          <div>Price: ${order?.price}</div>
+        </CardContent>
+        <CardActions className="menu-card-action" style={{flex: 1, textAlign: "center"}}>
+          <Button
+            size="small"
+            onClick={() => {
+              handleClick();
+              props?.handleAddItem(order);
+            }}
+            style={{width: "100%"}}
+          >
+            Add
+          </Button>
+          <Snackbar
+            open={open}
+            autoHideDuration={1000}
+            onClose={handleClose}
+            message="Added to cart"
+            action={action}
+          />
+        </CardActions>
+      </div>
     </Card>
   );
 }
